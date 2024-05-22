2b824682389b0ff30533be736b80545774599604
fbravo-telus <92489956+fbravo-telus@users.noreply.github.com>
2024-05-22 16:55:30 -0600

    Fixing import

--- java/com/benefits/appointments/models/enums/UserStatus.java
+++
@@ -1,6 +0,0 @@
-package com.benefits.appointments.models.enums;
-
-public enum UserStatus {
-
-  VERIFIED,PASSWORD_RESET,LOCKED,UPLOADED_NOT_REGISTERED
-}
--- java/com/benefits/appointments/repositories/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.benefits.appointments.repositories;
-
-import com.benefits.appointments.models.entities.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface PatientRepository extends JpaRepository<Patient, Long> {}
